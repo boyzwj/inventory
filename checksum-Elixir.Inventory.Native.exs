6a5ec892367e553cd797aeb1576d8c0ffff0c2dd
zhangwenjie <boyzwj@qq.com>
2024-07-06 19:01:18 +0800

    upgrade ci

---
+++ checksum-Elixir.Inventory.Native.exs
@@ -0,0 +1,12 @@
+%{
+  "inventory-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:40ee2845700611aab8d7d1f7507fe607a65eea60675c035d21fa26864365f058",
+  "inventory-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:f3ba3f73509a0aeac7e6d2c08d6d95171ed639483f10a46e1052f32693f07b11",
+  "libinventory-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:463a6d64fdac258d76895174e5a52572ad3fc8172ecccfc78594d606c5c760d2",
+  "libinventory-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:5e5672e0bfa13ae86c630bd12913d272ea7dd0b1003419d1b2f3ab739e6cb956",
+  "libinventory-v0.1.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:ac4308224d8fdcf1ff2b0b485f749d7e3c5c779d715eda3ab273af053b753594",
+  "libinventory-v0.1.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:7730e45a97ec883ec09416576fd5e348e06331623b9479f7aa18b96a71728db6",
+  "libinventory-v0.1.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:8bb986f719865729335c344b47ef207adfdecd2749b55d00e8f0d5d3719d6b27",
+  "libinventory-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:516b6be37ec76145c4a7161b8273a616b05594b175e8afe7e038215a09b36a1e",
+  "libinventory-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:470668ff03767427ad89cf4ef104f3ab3adb53e12078d408ed88f8c40886997c",
+  "libinventory-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:78453ef2ad1b3e8bbcb812e7e70d68b4ec4096176c9a96f317a92cf70ab207f5",
+}
