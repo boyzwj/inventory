9ab54f470507805173d81fa8d43b33ef7538924a
tom <tom@illusioncolors.com>
2024-12-07 09:05:54 +0000

    increase version

--- mix.exs
+++ mix.exs
@@ -1,6 +1,6 @@
 defmodule Inventory.MixProject do
   use Mix.Project
-  @version "0.1.0"
+  @version "0.2.0"
   @source_url "https://github.com/boyzwj/inventory"
 
   def project do
@@ -54,7 +54,7 @@ defmodule Inventory.MixProject do
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.2.0"}
       {:rustler_precompiled, "~> 0.7.0"},
       {:rustler, "~> 0.23", optional: true},
       {:benchee, "~> 1.3", only: [:test]}
